462048f6c7f154090c79181aeeac8bdc121539b9
Aman-kumar-gola-2006 <amanagola9841@gmail.com>
2026-03-09 10:24:41 +0530

    [Aman kumar gola] : added UC1 create contact functionality

--- src/main/java/com/addressbook/AddressBookMain.java
+++ src/main/java/com/addressbook/AddressBookMain.java
@@ -2,8 +2,21 @@ package com.addressbook;
 
 public class AddressBookMain {
 
-	public static void main(String[] args) {
-		
-		 System.out.println("Welcome to Address Book Program");
-	}
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Address Book Program");
+
+        Contact contact = new Contact(
+                "Aman",
+                "kumar gola",
+                "Bhopal",
+                "Bhopal",
+                "MP",
+                "462001",
+                "7409642260",
+                "AMANAGOLA9841@gmail.com"
+        );
+
+        contact.displayContact();
+    }
 }
